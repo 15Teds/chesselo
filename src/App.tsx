9eb76f82af277268cb3d24d4149e640affacc564
15Teds <teddy55johnson@gmail.com>
2023-01-24 19:33:22 -0500

    Fix: Footer overlap on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -153,7 +153,7 @@ function App() {
           </div>
         )}
 
-        <footer className="absolute bottom-0 flex justify-center items-center w-full h-20 bg-darkprimary text-white">
+        <footer className="flex md:fixed bottom-0 justify-center items-center w-full h-20 bg-darkprimary text-white">
           <p className="text-lg">
             Made by Ted - Not affiliated with{" "}
             <span className="text-greenprimary">chess.com</span>
